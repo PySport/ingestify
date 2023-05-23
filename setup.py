3e5d5373d8aa4a22c2e4f8533752774bd20d5595
Koen Vossen <info@koenvossen.nl>
2023-05-23 10:18:30 +0200

    Add missing dependencies/files

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
+import os
 from distutils.core import setup
 
 import setuptools
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
 
 
 def setup_package():
@@ -11,7 +20,20 @@ def setup_package():
         author_email="info@koenvossen.nl",
         license="AGPL",
         packages=setuptools.find_packages(exclude=["tests"]),
+        package_data={
+            '': package_files('ingestify/static'),
+        },
         entry_points={"console_scripts": ["ingestify = ingestify.cmdline:main"]},
+        install_requires=[
+            "requests>=2.0.0,<3",
+            "SQLAlchemy",
+            "dataclass_factory",
+            "cloudpickle",
+            "click",
+            "jinja2",
+            "python-dotenv",
+            "pyaml_env"
+        ],
     )
 
 
