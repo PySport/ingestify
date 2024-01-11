ea820b5aa9b67607ce78373dd848906ec04f7a91
Koen Vossen <info@koenvossen.nl>
2024-01-11 13:03:44 +0100

    Update imports

--- ingestify/domain/models/dataset/__init__.py
+++ ingestify/domain/models/dataset/__init__.py
@@ -3,6 +3,7 @@ from .dataset import Dataset
 from .dataset_repository import DatasetRepository, dataset_repository_factory
 from .file import DraftFile, File, LoadedFile
 from .file_repository import FileRepository, file_repository_factory
+from .file_collection import FileCollection
 from .identifier import Identifier
 from .selector import Selector
 from .revision import Revision
@@ -22,4 +23,5 @@ __all__ = [
     "DatasetRepository",
     "FileRepository",
     "file_repository_factory",
+    "FileCollection",
 ]
