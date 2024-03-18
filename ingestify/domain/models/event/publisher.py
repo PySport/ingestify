cc200d00d8a4892808ac1f835751385b2db04958
Koen Vossen <info@koenvossen.nl>
2024-03-18 11:30:03 +0100

    Minor

--- ingestify/domain/models/event/publisher.py
+++ ingestify/domain/models/event/publisher.py
@@ -1,6 +1,11 @@
+import logging
+
 from .dispatcher import Dispatcher
 from .domain_event import DomainEvent
 from .subscriber import Subscriber
+
+
+logger = logging.getLogger(__name__)
 
 
 class Publisher(Dispatcher):
@@ -9,7 +14,10 @@ class Publisher(Dispatcher):
 
     def dispatch(self, event: DomainEvent):
         for subscriber in self.subscribers:
-            subscriber.handle(event)
+            try:
+                subscriber.handle(event)
+            except Exception:
+                logger.exception(f"Failed to handle {event} by {subscriber}")
 
     def add_subscriber(self, subscriber: Subscriber):
         self.subscribers.append(subscriber)
