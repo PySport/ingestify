c79d5584b5df4bc4d75df92a104e47f28f8c8d88
Koen Vossen <info@koenvossen.nl>
2024-08-16 16:58:14 +0200

    Clean up

--- ingestify/tests/fake_source.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from typing import Optional, Dict, Union, List, Iterator
-
-import pytz
-
-from ingestify import Source
-from ingestify.domain import Identifier, DataSpecVersionCollection, Revision, DraftFile
