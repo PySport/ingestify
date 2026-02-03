358773c395659950d20a7f0f311195ff570c2f50
Koen Vossen <info@koenvossen.nl>
2026-02-03 09:35:11 +0100

    WIP

--- ingestify/tests/conftest.py
+++ ingestify/tests/conftest.py
@@ -12,6 +12,14 @@ def datastore_dir():
         yield tmpdirname
 
 
-@pytest.fixture(scope="session")
-def config_file():
+@pytest.fixture(scope="function", autouse=True)
+def ingestify_test_database_url(datastore_dir):
+    if not os.environ.get("INGESTIFY_TEST_DATABASE_URL"):
+        tmp_dir = os.environ["TEST_DIR"]
+        os.environ["INGESTIFY_TEST_DATABASE_URL"] = f"sqlite:///${tmp_dir}/main.db"
+
+
+@pytest.fixture(scope="function")
+def config_file(ingestify_test_database_url):
+    # Depend on ingestify_test_database_url to make sure environment variables are set in time
     return os.path.abspath(os.path.dirname(__file__) + "/config.yaml")
